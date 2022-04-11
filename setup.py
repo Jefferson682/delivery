2a6bb1d4c1c1913dddb84d769c07b1003471a454
J. Jefferson N. do Vale <criacao.jefferson@gmail.com>
2022-04-11 00:16:08 -0300

    created setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from gettext import find
+from setuptools import setup, find_packages
+
+
+def read(filename):
+    return [req.strip() for req in open(filename).readlines()]
+
+
+setup(
+    name="delivery",
+    author="J. Jefferson N. do Vale",
+    version="0.1.0",
+    description="Delivery app",
+    packages=find_packages(exclude="../.venv/"),
+    include_package_data=True,
+    install_requires=read("requirements.txt"),
+    extras_require={"dev": read("requirements-dev.txt")},
+)
