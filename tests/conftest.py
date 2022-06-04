65b84a452c97efcc424266679c9411255af8a609
J. Jefferson N. do Vale <criacao.jefferson@gmail.com>
2022-06-04 02:34:35 -0300

    add good practices

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,8 @@ import pytest
 
 from delivery.app import create_app
 
-@pytest.fixture(scope='module')
+
+@pytest.fixture(scope="module")
 def app():
     """Instance of Main flask app"""
     return create_app()
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,10 @@
 def test_app_is_created(app):
     assert app.name == "delivery.app"
-    
+
+
 def test_config_is_loaded(config):
-    assert config['DEBUG'] is True
-    
+    assert config["DEBUG"] is True
+
+
 def test_request_return_not_found(client):
     assert client.get("/url_not_exist").status_code == 404
