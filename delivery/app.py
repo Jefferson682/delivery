0b2d802f25447299bbcb6339aab27041652bda37
J. Jefferson N. do Vale <criacao.jefferson@gmail.com>
2022-06-06 00:36:28 -0300

    add config and debug extension

--- delivery/app.py
+++ delivery/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
-from .ext import site
+from .ext import site, debug, config
 
 
 def create_app():
     app = Flask(__name__)
+    config.init_app(app)
+    debug.init_app(app)
     site.init_app(app)
     return app
