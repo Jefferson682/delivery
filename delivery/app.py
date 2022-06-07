a6866ddcb0d9852eb203b5475c611f7fce024974
J. Jefferson N. do Vale <criacao.jefferson@gmail.com>
2022-06-07 20:58:45 -0300

    add ext migrate

--- delivery/app.py
+++ delivery/app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from .ext import site, toolbar, config, db, cli
+from .ext import site, toolbar, config, db, cli, migrate
 
 
 def create_app():
     app = Flask(__name__)
     config.init_app(app)
     db.init_app(app)
+    migrate.init_app(app)
     cli.init_app(app)
     toolbar.init_app(app)
     site.init_app(app)
