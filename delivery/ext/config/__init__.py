96640084271d0a7c869c84e69a3339179e1b3ea1
J. Jefferson N. do Vale <criacao.jefferson@gmail.com>
2022-06-07 21:00:48 -0300

    set SQLALCHEMY_TRACK_MODIFICATIONS=False

--- delivery/ext/config/__init__.py
+++ delivery/ext/config/__init__.py
@@ -1,6 +1,7 @@
 def init_app(app):
     app.config['SECRET_KEY'] = "xUBB0WbP1xclj8y7wQsGyZmKRaZNnXR2"
     app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///delivery.db'
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     
     if app.debug:
         app.config["DEBUG_TB_TEMPLATE_EDITOR_ENABLE"] = True
