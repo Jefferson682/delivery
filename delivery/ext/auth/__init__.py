3b79412834c46e890f7153599078f6df82a03294
J. Jefferson N. do Vale <criacao.jefferson@gmail.com>
2022-06-08 00:31:35 -0300

    add class UserAdmin

---
+++ delivery/ext/auth/__init__.py
@@ -0,0 +1,8 @@
+from delivery.ext.db import db
+from delivery.ext.admin import admin as main_admin
+from delivery.ext.auth.admin import UserAdmin
+from delivery.ext.site.models.models import User
+
+
+def init_app(app):
+    main_admin.add_view(UserAdmin(User, db.session))
