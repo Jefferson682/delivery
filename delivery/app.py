7a7dc72b334ae7dca15bbd956a82444866bcad10
J. Jefferson N. do Vale <criacao.jefferson@gmail.com>
2022-06-08 00:34:32 -0300

    add flask admin and update models

--- delivery/app.py
+++ delivery/app.py
@@ -1,13 +1,17 @@
 from flask import Flask
-from .ext import site, toolbar, config, db, cli, migrate
+from .ext import site, toolbar, config, db, cli, migrate, hooks, auth, admin
 
 
 def create_app():
     app = Flask(__name__)
     config.init_app(app)
     db.init_app(app)
+    auth.init_app(app)
+    admin.init_app(app)
     migrate.init_app(app)
     cli.init_app(app)
     toolbar.init_app(app)
     site.init_app(app)
+    
+    hooks.init_app(app)
     return app
--- delivery/ext/site/models/models.py
+++ delivery/ext/site/models/models.py
@@ -1,36 +1,41 @@
 from delivery.ext.db import db
 
+
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
     id = db.Column("id", db.Integer, primary_key=True)
     email = db.Column("email", db.Unicode, unique=True)
     passwd = db.Column("passwd", db.Unicode)
     admin = db.Column("admin", db.Boolean)
-    name = db.Column("name", db.Unicode)
+    username = db.Column("username", db.Unicode)
     
     def __repr__(self):
-        return self.name
-    
+        return f'{self.email}'
+
+
 class Category(db.Model):
-    __tablename__ = 'categories'
+    __tablename__ = "categories"
     id = db.Column("id", db.Integer, primary_key=True)
     name = db.Column("name", db.Unicode, unique=True)
     on_menu = db.Column("on_menu", db.Boolean)
     
     def __repr__(self):
-        return self.name
-    
+        return f'{self.name}'
+
+
 class Store(db.Model):
-    __tablename__ = 'stores'
+    __tablename__ = "stores"
     id = db.Column("id", db.Integer, primary_key=True)
     name = db.Column("name", db.Unicode, unique=True)
     user_id = db.Column("user_id", db.Integer, db.ForeignKey("users.id"))
     category_id = db.Column("category_id", db.Integer, db.ForeignKey("categories.id"))
-    active = db.Column("active", db.Boolean, default=True)
-    
+    is_active = db.Column("is_active", db.Boolean)
+
     users = db.relationship("User", foreign_keys=user_id)
     categories = db.relationship("Category", foreign_keys=category_id)
-    
+    def __repr__(self):
+        return f'{self.name}'
+
 class Item(db.Model):
     __tablename__ = "items"
     id = db.Column("id", db.Integer, primary_key=True)
@@ -39,9 +44,13 @@ class Item(db.Model):
     price = db.Column("price", db.Numeric)
     store_id = db.Column("store_id", db.Integer, db.ForeignKey("stores.id"))
     available = db.Column("available", db.Boolean)
-    
+
     stores = db.relationship("Store", foreign_keys=store_id)
-    
+
+    def __repr__(self):
+        return f'{self.name}'
+
+
 class Order(db.Model):
     __tablename__ = "orders"
     id = db.Column("id", db.Integer, primary_key=True)
@@ -50,21 +59,29 @@ class Order(db.Model):
     user_id = db.Column("user_id", db.Integer, db.ForeignKey("users.id"))
     store_id = db.Column("store_id", db.Integer, db.ForeignKey("stores.id"))
     address_id = db.Column("address", db.Integer, db.ForeignKey("address.id"))
-    
-    users = db.relationship("Users", foreign_keys=user_id)
+
+    users = db.relationship("User", foreign_keys=user_id)
     stores = db.relationship("Store", foreign_keys=store_id)
     address = db.relationship("Address", foreign_keys=address_id)
-    
+
+    def __repr__(self):
+        return f'Order: {self.id}'
+
+
 class OrderItems(db.Model):
     __tablename__ = "order_items"
     id = db.Column("id", db.Integer, primary_key=True)
     order_id = db.Column("order_id", db.Integer, db.ForeignKey("orders.id"))
     items_id = db.Column("items", db.Integer, db.ForeignKey("items.id"))
     qtd = db.Column("qtd", db.Integer)
-    
+
     orders = db.relationship("Order", foreign_keys=order_id)
     items = db.relationship("Item", foreign_keys=items_id)
-    
+
+    def __repr__(self):
+        return f'Order Items: {self.id}'
+
+
 class Checkout(db.Model):
     __tablename__ = "checkouts"
     id = db.Column("id", db.Integer, primary_key=True)
@@ -73,15 +90,16 @@ class Checkout(db.Model):
     created_at = db.Column("created_at", db.DateTime)
     completed = db.Column("completed", db.Boolean)
     order_id = db.Column("order_id", db.Integer, db.ForeignKey("orders.id"))
-    
-    orders = db.relationship("Orders", foreign_keys="order_id")
-    
-class address(db.Model):
+
+    orders = db.relationship("Order", foreign_keys=order_id)
+
+
+class Address(db.Model):
     __tablename__ = "address"
     id = db.Column("id", db.Integer, primary_key=True)
     zip = db.Column("zip", db.Unicode)
     country = db.Column("country", db.Unicode)
     address = db.Column("address", db.Unicode)
     user_id = db.Column("user_id", db.Integer, db.ForeignKey("users.id"))
-    
+
     users = db.relationship("User", foreign_keys=user_id)
